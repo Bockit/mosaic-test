972943f4564ce2d9e71f19f0081d15fa50888693
James Hiscock <james@small.mu>
2015-08-29 15:53:08 +1000

    Move non-server settings out of shared client-server settings

--- js/client.js
+++ js/client.js
@@ -2,6 +2,9 @@ var settings = require('./mosaic')
 var FileDragger = require('./lib/file-dragger')
 var mosaic = require('./lib/mosaic')
 
+settings.ALLOWED_TYPES = [ 'image/png', 'image/jpeg' ];
+settings.BASE_URL = '/'
+
 var dragger = FileDragger()
 dragger.on('file', function (file) {
     if (settings.ALLOWED_TYPES.indexOf(file.type) === -1) return
