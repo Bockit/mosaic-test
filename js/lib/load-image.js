1aa8c0d657c1c471a8acfbc3ea846bc3a6b42ec5
James Hiscock <james@small.mu>
2015-08-30 12:17:50 +1000

    Fixed comment

--- js/lib/load-image.js
+++ js/lib/load-image.js
@@ -3,7 +3,7 @@ var memoize = require('lodash/function/memoize')
 module.exports = memoize(loadImage)
 
 /**
- * Takes a url and returns a promise that resolves to a loaded img object.
+ * Takes a url and returns a promise that resolves to a loaded Image object.
  */
 function loadImage(url) {
     return new Promise(function (resolve, reject) {
